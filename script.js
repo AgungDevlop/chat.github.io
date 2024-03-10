7eda890f322d3f791097dc25815d804abd336e5c
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2024-03-10 15:44:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,207 @@
-
-    if (window.location.hostname !== 'agungdevlop.github.io') {
-        // Redirect ke URL yang diinginkan
-        window.location.href = 'https://agungdevlop.github.io/chat.github.io/';
+function _0x5d1a(_0x40779d, _0x169761) {
+    const _0x5807a4 = _0x5d05();
+    return _0x5d1a = function (_0xeff74f, _0x2f6c76) {
+        _0xeff74f = _0xeff74f - (0x2185 * 0x1 + 0x817 + -0x28f3);
+        let _0x170eaf = _0x5807a4[_0xeff74f];
+        return _0x170eaf;
+    }, _0x5d1a(_0x40779d, _0x169761);
+}
+const _0x3564d7 = _0x5d1a;
+function _0x5d05() {
+    const _0x37f1d8 = [
+        'ebook-mess',
+        'fab\x20fa-wha',
+        'TBvPR',
+        'notificati',
+        'zCcey',
+        'fab\x20fa-fac',
+        'showNotifi',
+        'trim',
+        'location',
+        'odskep.my.',
+        'ications:',
+        'jFlZi',
+        '1137615ObsQkm',
+        'telegram-b',
+        '91595doQqzA',
+        'pxLQD',
+        'DKBcs',
+        'loading',
+        'n/json',
+        'egram',
+        'tsapp',
+        'agungdevlo',
+        'dfNci',
+        'chat.githu',
+        '612578bNjBNC',
+        'searchInpu',
+        'pKmed',
+        '14512WPoXZv',
+        'Error\x20fetc',
+        'b.io/',
+        '4167Tgbidm',
+        'applicatio',
+        'length',
+        '15224FZBWga',
+        'ications',
+        'Mufzt',
+        'HwhQE',
+        'toLowerCas',
+        'hing\x20notif',
+        'OZuRG',
+        'enger',
+        'sender',
+        'href',
+        'json',
+        'https://ag',
+        'id/notifre',
+        'p.github.i',
+        'quest.php',
+        '6lbJyNx',
+        'Telegram',
+        'koBco',
+        'App',
+        'POST',
+        'messenger-',
+        'pAOSB',
+        'usiness',
+        'sDyee',
+        'whatsapp-b',
+        'https://do',
+        'hostname',
+        'fab\x20fa-tel',
+        'WhatsApp\x20B',
+        'fetchNotif',
+        'nohp',
+        '#app',
+        '542728ZicbED',
+        'GIjda',
+        'includes',
+        'usiness-bg',
+        'error',
+        '54dSkmwr',
+        '2270EVxYkL',
+        '3318312IvCvSk',
+        'IDVZl',
+        'LDqTL',
+        'filter',
+        'oDKEW',
+        'WhatsApp',
+        'tQNGV',
+        'ungdevlop.',
+        'github.io/',
+        'cations',
+        'AgungDev',
+        'group',
+        'atNLP',
+        'OPVGU',
+        'ons',
+        'zOyeT',
+        'Messenger'
+    ];
+    _0x5d05 = function () {
+        return _0x37f1d8;
+    };
+    return _0x5d05();
+}
+(function (_0x23434a, _0x51ca4d) {
+    const _0x59310d = _0x5d1a, _0x3b3045 = _0x23434a();
+    while (!![]) {
+        try {
+            const _0x3fb392 = parseInt(_0x59310d(0xfc)) / (0x1 * 0x15e1 + -0xc0a + -0x9d6) + parseInt(_0x59310d(0xcc)) / (0x188 * 0xa + 0x17b * -0x7 + -0x4f1) * (parseInt(_0x59310d(0xbb)) / (0x6 * -0x2e3 + -0xd11 + 0x1e66)) + parseInt(_0x59310d(0xd3)) / (0x22e6 + -0x117b + -0x21 * 0x87) + -parseInt(_0x59310d(0xf0)) / (0xae7 * 0x2 + -0x2176 + 0xbad) + parseInt(_0x59310d(0xd1)) / (0x1d7f + 0x195d + -0x1b6b * 0x2) * (-parseInt(_0x59310d(0xf2)) / (0x14a + -0x295 * 0xd + 0x204e)) + -parseInt(_0x59310d(0xff)) / (-0x2 * -0x22 + 0x157b + -0x6d * 0x33) * (parseInt(_0x59310d(0xa9)) / (0x1c56 + 0x2b * 0xa9 + -0x38b0)) + -parseInt(_0x59310d(0xd2)) / (-0x12 * -0x59 + -0x26ce * -0x1 + -0x781 * 0x6) * (parseInt(_0x59310d(0xac)) / (0x1bc2 + 0x9be * 0x3 + 0x71 * -0x81));
+            if (_0x3fb392 === _0x51ca4d)
+                break;
+            else
+                _0x3b3045['push'](_0x3b3045['shift']());
+        } catch (_0xb5322e) {
+            _0x3b3045['push'](_0x3b3045['shift']());
+        }
     }
-    
-        new Vue({
-            el: '#app',
-            data() {
-                return {
-                    searchInput: '',
-                    notifications: [],
-                    showNotifications: false,
-                    loading: false
+}(_0x5d05, -0x85ebf + 0x2 * 0x3921a + 0x8a335), (_0x3564d7(0xf9) + _0x3564d7(0xb9) + 'o' !== window[_0x3564d7(0xec)][_0x3564d7(0xc6)] && (window[_0x3564d7(0xec)][_0x3564d7(0xb5)] = _0x3564d7(0xb7) + _0x3564d7(0xda) + _0x3564d7(0xdb) + _0x3564d7(0xfb) + _0x3564d7(0x101)), new Vue({
+    'el': _0x3564d7(0xcb),
+    'data': () => ({
+        'searchInput': '',
+        'notifications': [],
+        'showNotifications': !(0x2474 + -0x125b + -0x1218),
+        'loading': !(0x1fe5 + 0x288 + -0x226c)
+    }),
+    'methods': {
+        async 'fetchNotifications'() {
+            const _0x45a698 = _0x3564d7, _0x4d424e = {
+                    'OZuRG': function (_0x126a73, _0x4d4f59, _0x1e1cbc) {
+                        return _0x126a73(_0x4d4f59, _0x1e1cbc);
+                    },
+                    'zOyeT': _0x45a698(0xc5) + _0x45a698(0xed) + _0x45a698(0xb8) + _0x45a698(0xba),
+                    'tQNGV': _0x45a698(0xbf),
+                    'jFlZi': _0x45a698(0xaa) + _0x45a698(0xf6),
+                    'dfNci': _0x45a698(0xdd),
+                    'IDVZl': _0x45a698(0x100) + _0x45a698(0xb1) + _0x45a698(0xee)
                 };
-            },
-            methods: {
-                async fetchNotifications() {
-                    try {
-                        const response = await fetch('https://doodskep.my.id/notifrequest.php', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'X-API-Key': 'AgungDev' // Ganti dengan kunci API yang sesuai
-                            }
-                        });
-                        const data = await response.json();
-                        this.notifications = data;
-                    } catch (error) {
-                        console.error('Error fetching notifications:', error);
-                        this.notifications = [];
-                    }
-                },
-                async searchNotifications() {
-                    const searchValue = this.searchInput.trim().toLowerCase();
-
-                    // Tampilkan loading spinner
-                    this.loading = true;
-
-                    // Ambil semua notifikasi dari API
-                    await this.fetchNotifications();
-
-                    // Filter notifikasi berdasarkan nomor telepon yang dicari
-                    this.notifications = this.notifications.filter(notification => {
-                        return notification.nohp.includes(searchValue);
-                    });
-
-                    // Sembunyikan loading spinner
-                    this.loading = false;
-
-                    // Tampilkan notifikasi setelah pencarian
-                    this.showNotifications = true;
-                },
-                getNotificationClass(notification) {
-                    switch (notification.notifications[0].App) {
-                        case 'WhatsApp':
-                            return 'whatsapp-bg';
-                        case 'Messenger':
-                            return 'messenger-bg';
-                        case 'Telegram':
-                            return 'telegram-bg';
-                        case 'WhatsApp Business':
-                            return 'whatsapp-business-bg';
-                        default:
-                            return '';
-                    }
-                },
-                getAppIconClass(app) {
-                    switch (app) {
-                        case 'WhatsApp':
-                            return 'fab fa-whatsapp';
-                        case 'Messenger':
-                            return 'fab fa-facebook-messenger';
-                        case 'Telegram':
-                            return 'fab fa-telegram';
-                        case 'WhatsApp Business':
-                            return 'fab fa-whatsapp';
-                        default:
-                            return '';
-                    }
-                },
-                getTotalGroup(notifications) {
-                    return notifications.filter(notif => notif.group).length;
-                },
-                getTotalSender(notifications) {
-                    return notifications.filter(notif => notif.sender).length;
-                },
-                getTotalMessages(notifications) {
-                    return notifications.length;
-                }
+            try {
+                const _0x19ac34 = await _0x4d424e[_0x45a698(0xb2)](fetch, _0x4d424e[_0x45a698(0xe2)], {
+                        'method': _0x4d424e[_0x45a698(0xd9)],
+                        'headers': {
+                            'Content-Type': _0x4d424e[_0x45a698(0xef)],
+                            'X-API-Key': _0x4d424e[_0x45a698(0xfa)]
+                        }
+                    }), _0x1f5ab6 = await _0x19ac34[_0x45a698(0xb6)]();
+                this[_0x45a698(0xe7) + _0x45a698(0xe1)] = _0x1f5ab6;
+            } catch (_0x57716c) {
+                console[_0x45a698(0xd0)](_0x4d424e[_0x45a698(0xd4)], _0x57716c), this[_0x45a698(0xe7) + _0x45a698(0xe1)] = [];
             }
-        });
-        
-    
+        },
+        async 'searchNotifications'() {
+            const _0x329255 = _0x3564d7, _0x4d9f6f = this[_0x329255(0xfd) + 't'][_0x329255(0xeb)]()[_0x329255(0xb0) + 'e']();
+            this[_0x329255(0xf5)] = !(0x194b + -0x610 * -0x4 + -0x481 * 0xb), await this[_0x329255(0xc9) + _0x329255(0xad)](), this[_0x329255(0xe7) + _0x329255(0xe1)] = this[_0x329255(0xe7) + _0x329255(0xe1)][_0x329255(0xd6)](_0x554274 => _0x554274[_0x329255(0xca)][_0x329255(0xce)](_0x4d9f6f)), this[_0x329255(0xf5)] = !(-0x12d4 + 0x1c11 + -0x93c), this[_0x329255(0xea) + _0x329255(0xdc)] = !(-0x109 * -0x1 + 0x12b3 + -0x13bc);
+        },
+        'getNotificationClass'(_0x149a72) {
+            const _0x506bbb = _0x3564d7, _0x327e9c = {
+                    'TBvPR': _0x506bbb(0xd8),
+                    'sDyee': _0x506bbb(0xc4) + 'g',
+                    'pxLQD': _0x506bbb(0xe3),
+                    'DKBcs': _0x506bbb(0xc0) + 'bg',
+                    'GIjda': _0x506bbb(0xbc),
+                    'Mufzt': _0x506bbb(0xf1) + 'g',
+                    'HwhQE': _0x506bbb(0xc8) + _0x506bbb(0xc2),
+                    'atNLP': _0x506bbb(0xc4) + _0x506bbb(0xcf)
+                };
+            switch (_0x149a72[_0x506bbb(0xe7) + _0x506bbb(0xe1)][0x1ea8 + -0x26c2 + 0x2 * 0x40d][_0x506bbb(0xbe)]) {
+            case _0x327e9c[_0x506bbb(0xe6)]:
+                return _0x327e9c[_0x506bbb(0xc3)];
+            case _0x327e9c[_0x506bbb(0xf3)]:
+                return _0x327e9c[_0x506bbb(0xf4)];
+            case _0x327e9c[_0x506bbb(0xcd)]:
+                return _0x327e9c[_0x506bbb(0xae)];
+            case _0x327e9c[_0x506bbb(0xaf)]:
+                return _0x327e9c[_0x506bbb(0xdf)];
+            default:
+                return '';
+            }
+        },
+        'getAppIconClass'(_0x5d05ce) {
+            const _0x14210e = _0x3564d7, _0x2c7390 = {
+                    'pAOSB': _0x14210e(0xd8),
+                    'OPVGU': _0x14210e(0xc8) + _0x14210e(0xc2),
+                    'pKmed': _0x14210e(0xe5) + _0x14210e(0xf8),
+                    'koBco': _0x14210e(0xe3),
+                    'LDqTL': _0x14210e(0xe9) + _0x14210e(0xe4) + _0x14210e(0xb3),
+                    'zCcey': _0x14210e(0xbc),
+                    'oDKEW': _0x14210e(0xc7) + _0x14210e(0xf7)
+                };
+            switch (_0x5d05ce) {
+            case _0x2c7390[_0x14210e(0xc1)]:
+            case _0x2c7390[_0x14210e(0xe0)]:
+                return _0x2c7390[_0x14210e(0xfe)];
+            case _0x2c7390[_0x14210e(0xbd)]:
+                return _0x2c7390[_0x14210e(0xd5)];
+            case _0x2c7390[_0x14210e(0xe8)]:
+                return _0x2c7390[_0x14210e(0xd7)];
+            default:
+                return '';
+            }
+        },
+        'getTotalGroup': _0x390a6b => _0x390a6b[_0x3564d7(0xd6)](_0x4d3e76 => _0x4d3e76[_0x3564d7(0xde)])[_0x3564d7(0xab)],
+        'getTotalSender': _0x58786d => _0x58786d[_0x3564d7(0xd6)](_0x326a55 => _0x326a55[_0x3564d7(0xb4)])[_0x3564d7(0xab)],
+        'getTotalMessages': _0x1750a4 => _0x1750a4[_0x3564d7(0xab)]
+    }
+})));
