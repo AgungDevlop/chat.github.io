8465ff0884cad15619e8cb333f9f2131798ef15b
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2024-03-13 14:20:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,84 +1,201 @@
-
-        
-
-        new Vue({
-            el: '#app',
-            data() {
-                return {
-                    searchInput: '',
-                    notifications: [],
-                    showNotifications: false,
-                    loading: false
+const _0x34e5c4 = _0x2ddc;
+function _0x2ddc(_0xdf82cd, _0x31869e) {
+    const _0x4fd5e1 = _0x491d();
+    return _0x2ddc = function (_0x1f0d39, _0x590e5d) {
+        _0x1f0d39 = _0x1f0d39 - (0x5c9 + 0x15e + -0x69d);
+        let _0x224afb = _0x4fd5e1[_0x1f0d39];
+        return _0x224afb;
+    }, _0x2ddc(_0xdf82cd, _0x31869e);
+}
+(function (_0x50faad, _0x3461b4) {
+    const _0x16f6c7 = _0x2ddc, _0x581eb6 = _0x50faad();
+    while (!![]) {
+        try {
+            const _0x2c977c = -parseInt(_0x16f6c7(0xc9)) / (0x1e52 + 0xd5 + 0x1f26 * -0x1) + parseInt(_0x16f6c7(0xdd)) / (0x19 * -0x9e + 0xa3 * 0xc + 0x7cc) + -parseInt(_0x16f6c7(0x9e)) / (0x237f + 0x1e5a + -0x3 * 0x15f2) + parseInt(_0x16f6c7(0xc7)) / (-0x115c + -0x4 * -0x621 + -0x392 * 0x2) + parseInt(_0x16f6c7(0xba)) / (0x21c9 + 0x1cd8 + 0xfa7 * -0x4) + parseInt(_0x16f6c7(0xc5)) / (0x1cdd + 0x7c7 * -0x3 + -0x582) + parseInt(_0x16f6c7(0xd6)) / (0x2218 + -0x5b2 * 0x3 + 0x17 * -0xbd) * (-parseInt(_0x16f6c7(0x8d)) / (-0x1f * -0x21 + 0x5c * -0x3f + 0x12ad));
+            if (_0x2c977c === _0x3461b4)
+                break;
+            else
+                _0x581eb6['push'](_0x581eb6['shift']());
+        } catch (_0xcde4ed) {
+            _0x581eb6['push'](_0x581eb6['shift']());
+        }
+    }
+}(_0x491d, -0xc * 0x63be + -0x28e1 * -0x1d + 0x124bb * 0x6), (_0x34e5c4(0xc4) + _0x34e5c4(0xd8) !== window[_0x34e5c4(0x96)][_0x34e5c4(0x8f)] && (window[_0x34e5c4(0x96)][_0x34e5c4(0xbe)] = _0x34e5c4(0xcd) + _0x34e5c4(0xbd) + _0x34e5c4(0xa0)), new Vue({
+    'el': _0x34e5c4(0xd0),
+    'data': () => ({
+        'searchInput': '',
+        'notifications': [],
+        'showNotifications': !(-0x10b7 + 0x2 * 0xa06 + 0xc * -0x47),
+        'loading': !(0x488 + -0x19a2 + -0x709 * -0x3)
+    }),
+    'methods': {
+        async 'fetchNotifications'() {
+            const _0x12bd11 = _0x34e5c4, _0x355abf = {
+                    'JLtqA': function (_0x74be52, _0xe8e0cb, _0x541de9) {
+                        return _0x74be52(_0xe8e0cb, _0x541de9);
+                    },
+                    'DbAFJ': _0x12bd11(0x9b) + _0x12bd11(0xbc) + _0x12bd11(0xc8) + _0x12bd11(0x9c),
+                    'YcoGQ': _0x12bd11(0xdb),
+                    'QxrKa': _0x12bd11(0xca) + _0x12bd11(0xcc),
+                    'exkyn': _0x12bd11(0xd5),
+                    'KTvHx': _0x12bd11(0xd9) + _0x12bd11(0x99) + _0x12bd11(0x8a)
                 };
-            },
-            methods: {
-                async fetchNotifications() {
-                    try {
-                        const response = await fetch('https://doodskep.my.id/notifrequest.php', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'X-API-Key': 'AgungDev' // Ganti dengan kunci API yang sesuai
-                            }
-                        });
-                        const data = await response.json();
-                        this.notifications = data.map(item => {
-                            return {
-                                nohp: item.nohp,
-                                notifications: item.notifications.reverse()
-                            };
-                        });
-                    } catch (error) {
-                        console.error('Error fetching notifications:', error);
-                        this.notifications = [];
-                    }
-                },
-                async searchNotifications() {
-                    const searchValue = this.searchInput.trim().toLowerCase();
-
-                    // Tampilkan loading spinner
-                    this.loading = true;
-
-                    // Ambil semua notifikasi dari API
-                    await this.fetchNotifications();
-
-                    // Filter notifikasi berdasarkan nomor telepon yang dicari
-                    this.notifications = this.notifications.filter(notification => {
-                        return notification.nohp.includes(searchValue);
-                    });
-
-                    // Sembunyikan loading spinner
-                    this.loading = false;
-
-                    // Tampilkan notifikasi setelah pencarian
-                    this.showNotifications = true;
-                },
-                getAppIconClass(app) {
-                    switch (app) {
-                        case 'WhatsApp':
-                            return 'fab fa-whatsapp';
-                        case 'Messenger':
-                            return 'fab fa-facebook-messenger';
-                        case 'Telegram':
-                            return 'fab fa-telegram';
-                        case 'WhatsApp Business':
-                            return 'fab fa-whatsapp';
-                        default:
-                            return '';
-                    }
-                },
-                formatTime(time) {
-                    const [hourMinute, day, month, year] = time.split(',');
-                    const [hour, minute] = hourMinute.split(':');
-                    return `${hour}:${minute} ${day} ${this.getMonthName(parseInt(month))} ${year}`;
-                },
-                getMonthName(month) {
-                    const months = [
-                        'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
-                    ];
-                    return months[month - 1];
-                }
+            try {
+                const _0x23c441 = await _0x355abf[_0x12bd11(0x90)](fetch, _0x355abf[_0x12bd11(0xbf)], {
+                        'method': _0x355abf[_0x12bd11(0x98)],
+                        'headers': {
+                            'Content-Type': _0x355abf[_0x12bd11(0x8c)],
+                            'X-API-Key': _0x355abf[_0x12bd11(0x9a)]
+                        }
+                    }), _0x27ca3f = await _0x23c441[_0x12bd11(0xde)]();
+                this[_0x12bd11(0xc1) + _0x12bd11(0xb6)] = _0x27ca3f[_0x12bd11(0x95)](_0x1ba77e => ({
+                    'nohp': _0x1ba77e[_0x12bd11(0xb0)],
+                    'notifications': _0x1ba77e[_0x12bd11(0xc1) + _0x12bd11(0xb6)][_0x12bd11(0xaa)]()
+                }));
+            } catch (_0x4d3b53) {
+                console[_0x12bd11(0xb3)](_0x355abf[_0x12bd11(0xb7)], _0x4d3b53), this[_0x12bd11(0xc1) + _0x12bd11(0xb6)] = [];
             }
-        });
-
-    
+        },
+        async 'searchNotifications'() {
+            const _0x51b001 = _0x34e5c4, _0x2f321b = this[_0x51b001(0xad) + 't'][_0x51b001(0xb5)]()[_0x51b001(0xb9) + 'e']();
+            this[_0x51b001(0x91)] = !(0x12cf + 0xea2 * 0x1 + -0x2171), await this[_0x51b001(0xa4) + _0x51b001(0xc6)](), this[_0x51b001(0xc1) + _0x51b001(0xb6)] = this[_0x51b001(0xc1) + _0x51b001(0xb6)][_0x51b001(0xd2)](_0x2d176b => _0x2d176b[_0x51b001(0xb0)][_0x51b001(0x9f)](_0x2f321b)), this[_0x51b001(0x91)] = !(0xeee + 0x16e4 + -0x567 * 0x7), this[_0x51b001(0xa5) + _0x51b001(0xaf)] = !(-0x1b0b + -0x1b3b + -0x1b23 * -0x2);
+        },
+        'getAppIconClass'(_0x17301a) {
+            const _0x1dc18e = _0x34e5c4, _0x4b38cf = {
+                    'KRHgh': _0x1dc18e(0xae),
+                    'vAUgX': _0x1dc18e(0xa6) + _0x1dc18e(0xac),
+                    'FWVNB': _0x1dc18e(0xa8) + _0x1dc18e(0x97),
+                    'BPapz': _0x1dc18e(0xd3),
+                    'rypZx': _0x1dc18e(0xa2) + _0x1dc18e(0xab) + _0x1dc18e(0xb2),
+                    'qQYhd': _0x1dc18e(0xcf),
+                    'cjJWO': _0x1dc18e(0xce) + _0x1dc18e(0xda)
+                };
+            switch (_0x17301a) {
+            case _0x4b38cf[_0x1dc18e(0x93)]:
+            case _0x4b38cf[_0x1dc18e(0xb8)]:
+                return _0x4b38cf[_0x1dc18e(0xc2)];
+            case _0x4b38cf[_0x1dc18e(0xbb)]:
+                return _0x4b38cf[_0x1dc18e(0xd1)];
+            case _0x4b38cf[_0x1dc18e(0xa3)]:
+                return _0x4b38cf[_0x1dc18e(0x94)];
+            default:
+                return '';
+            }
+        },
+        'formatTime'(_0x56d98b) {
+            const _0x478272 = _0x34e5c4, _0x322970 = {
+                    'bIKPP': function (_0x4dfda7, _0x213565) {
+                        return _0x4dfda7(_0x213565);
+                    }
+                }, [_0x25ebd1, _0x34e305, _0x473308, _0x40ab9a] = _0x56d98b[_0x478272(0xc3)](','), [_0x42dff2, _0x7d2ade] = _0x25ebd1[_0x478272(0xc3)](':');
+            return _0x42dff2 + ':' + _0x7d2ade + '\x20' + _0x34e305 + '\x20' + this[_0x478272(0xc0) + 'me'](_0x322970[_0x478272(0xdc)](parseInt, _0x473308)) + '\x20' + _0x40ab9a;
+        },
+        'getMonthName': _0x313396 => [
+            _0x34e5c4(0xd4),
+            _0x34e5c4(0x8e),
+            _0x34e5c4(0x92),
+            _0x34e5c4(0xa9),
+            _0x34e5c4(0xa7),
+            _0x34e5c4(0xb4),
+            _0x34e5c4(0xb1),
+            _0x34e5c4(0xd7),
+            _0x34e5c4(0x8b),
+            _0x34e5c4(0x9d),
+            _0x34e5c4(0xcb),
+            _0x34e5c4(0xa1)
+        ][_0x313396 - (0x2 * 0xa1a + -0x6cd + -0xd66)]
+    }
+})));
+function _0x491d() {
+    const _0x4ee52e = [
+        'Oktober',
+        '328629IszbVF',
+        'includes',
+        'ce.my.id/',
+        'Desember',
+        'fab\x20fa-fac',
+        'qQYhd',
+        'fetchNotif',
+        'showNotifi',
+        'WhatsApp\x20B',
+        'Mei',
+        'fab\x20fa-wha',
+        'April',
+        'reverse',
+        'ebook-mess',
+        'usiness',
+        'searchInpu',
+        'WhatsApp',
+        'cations',
+        'nohp',
+        'Juli',
+        'enger',
+        'error',
+        'Juni',
+        'trim',
+        'ons',
+        'KTvHx',
+        'vAUgX',
+        'toLowerCas',
+        '616445bRdJyl',
+        'BPapz',
+        'odskep.my.',
+        'ats.apkspa',
+        'href',
+        'DbAFJ',
+        'getMonthNa',
+        'notificati',
+        'FWVNB',
+        'split',
+        'chats.apks',
+        '2774742QjRFGl',
+        'ications',
+        '1706828XczDYv',
+        'id/notifre',
+        '579174YEbjoY',
+        'applicatio',
+        'November',
+        'n/json',
+        'https://ch',
+        'fab\x20fa-tel',
+        'Telegram',
+        '#app',
+        'rypZx',
+        'filter',
+        'Messenger',
+        'Januari',
+        'AgungDev',
+        '7ULPtUe',
+        'Agustus',
+        'pace.my.id',
+        'Error\x20fetc',
+        'egram',
+        'POST',
+        'bIKPP',
+        '362956OqiMDL',
+        'json',
+        'ications:',
+        'September',
+        'QxrKa',
+        '468024LYkhBL',
+        'Februari',
+        'hostname',
+        'JLtqA',
+        'loading',
+        'Maret',
+        'KRHgh',
+        'cjJWO',
+        'map',
+        'location',
+        'tsapp',
+        'YcoGQ',
+        'hing\x20notif',
+        'exkyn',
+        'https://do',
+        'quest.php'
+    ];
+    _0x491d = function () {
+        return _0x4ee52e;
+    };
+    return _0x491d();
+}
